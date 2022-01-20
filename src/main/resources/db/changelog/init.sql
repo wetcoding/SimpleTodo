50570eda3551f9c0c048827f0acb0dbb9fe827e0
Ravil Mashakaev <ravil.mashakaev@lamoda.ru>
2022-01-20 16:19:58 +0300

    add schema initial state

--- src/main/resources/db/changelog/init.sql
+++ src/main/resources/db/changelog/init.sql
@@ -1,3 +1,35 @@
 --liquibase formatted sql
 --changeset ravil.mashakaev:create-id-sequence
 create sequence id_sequence start 1;
+
+--changeset ravil.mashakaev:create-user-table
+create table user_info
+(
+    id            bigint
+        CONSTRAINT pk_user PRIMARY KEY,
+    created       timestamp    NOT NULL,
+    modified      timestamp    NOT NULL,
+    version       int          NOT NULL,
+    first_name    varchar(255) NOT NULL,
+    last_name     varchar(255) NOT NULL,
+    email         varchar(255) NOT NULL
+        CONSTRAINT user_email_uniq UNIQUE,
+    password_hash varchar(255) NOT NULL
+);
+
+create index idx_user_email on user_info (email);
+
+--changeset ravil.mashakaev:create-todo-list-table
+create table todo_list
+(
+    id       bigint
+        CONSTRAINT pk_todo_list PRIMARY KEY,
+    created  timestamp    NOT NULL,
+    modified timestamp    NOT NULL,
+    version  int          NOT NULL,
+    title    varchar(255) NOT NULL,
+    body     text,
+    status   varchar(255) NOT NULL,
+    user_id  bigint       NOT NULL
+        CONSTRAINT fk_user_id REFERENCES user_info
+)
